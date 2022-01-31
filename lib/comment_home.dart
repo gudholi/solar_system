8c5b314f83d81cd8d631ab30f39c55a8a68688bf
DELL <sylpro3@gmail.com>
2022-01-31 12:11:34 +0600

    Text_Field_and_snakbar_done

--- lib/comment_home.dart
+++ lib/comment_home.dart
@@ -21,17 +21,30 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController _controller = TextEditingController();
   //var now;
   //final _formKey = GlobalKey<FormState>();
+  bool _validate = false;
   int? selectId;
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: gradientEndColor,
+      //backgroundColor: Colors.blue,
       floatingActionButton: ElevatedButton(
         onPressed: () {
           print(_controller.text);
           setState(() {
+            _controller.text.isEmpty ? _validate = true : _validate = false;
+
             if (selectId == null && _controller.text.isNotEmpty) {
               DatabaseHelper.instance.add(Grocery(name: _controller.text));
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Data Inserted Successfully')),
+              );
             } else {
               DatabaseHelper.instance
                   .update(Grocery(name: _controller.text, id: selectId));
@@ -49,56 +62,65 @@ class _HomeScreenState extends State<HomeScreen> {
             decoration: InputDecoration(
               //border: OutlineInputBorder(),
               labelText: 'Write Comment Here',
+              errorText: _validate ? 'Value Can\'t Be Empty' : null,
             ),
             controller: _controller,
           ),
         ),
         backgroundColor: Colors.blueGrey,
       ),
-      body: FutureBuilder<List<Grocery>>(
-        future: DatabaseHelper.instance.getGrocery(),
-        builder: (context, snapshot) {
-          if (snapshot.data!.isEmpty && _controller.text.isNotEmpty) {
-            //print('Not Found');
-            return Center(
-              child: Text('no List Found'),
-            );
-          } /*else if (_controller.text != null || _controller.text.isEmpty) {
-            print('');
-          }*/
-          else {
-            return ListView(
-              children: snapshot.data!.map((e) {
-                return Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32),
-                    ),
-                    child: ListTile(
-                      onLongPress: () {
-                        setState(() {
-                          DatabaseHelper.instance.remove(e.id!);
-                        });
-                      },
-                      onTap: () {
-                        //var now = DateTime.now();
-                        _controller.text = e.name;
-                        selectId = e.id;
-                      },
-                      title: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(e.name),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [gradientStartColor, gradientEndColor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.3, 0.7])),
+        child: FutureBuilder<List<Grocery>>(
+          future: DatabaseHelper.instance.getGrocery(),
+          builder: (context, snapshot) {
+            if (snapshot.data!.isEmpty && _controller.text.isNotEmpty) {
+              //print('Not Found');
+              return Center(
+                child: Text('no List Found'),
+              );
+            } /*else if (_controller.text != null || _controller.text.isEmpty) {
+              print('');
+            }*/
+            else {
+              return ListView(
+                children: snapshot.data!.map((e) {
+                  return Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
                       ),
-                      //subtitle: Text('now'),
-                      //subtitle: Text(e.id.toString()),
+                      child: ListTile(
+                        onLongPress: () {
+                          setState(() {
+                            DatabaseHelper.instance.remove(e.id!);
+                          });
+                        },
+                        onTap: () {
+                          //var now = DateTime.now();
+                          _controller.text = e.name;
+                          selectId = e.id;
+                        },
+                        title: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(e.name),
+                        ),
+                        //subtitle: Text('now'),
+                        //subtitle: Text(e.id.toString()),
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            );
-          }
-        },
+                  );
+                }).toList(),
+              );
+            }
+          },
+        ),
       ),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -16,93 +16,102 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: gradientEndColor,
       body: SafeArea(
         bottom: false,
         child: Stack(
           children: <Widget>[
             SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(32.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(height: 300),
-                        Text(
-                          planetInfo.name,
-                          style: TextStyle(
-                            fontFamily: 'Avenir',
-                            fontSize: 56,
-                            color: primaryTextColor,
-                            fontWeight: FontWeight.w900,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Text(
-                          'Solar System',
-                          style: TextStyle(
-                            fontFamily: 'Avenir',
-                            fontSize: 31,
-                            color: primaryTextColor,
-                            fontWeight: FontWeight.w300,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Divider(color: Colors.black38),
-                        SizedBox(height: 32),
-                        Text(
-                          planetInfo.description, //?? '',
-                          maxLines: 5,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontFamily: 'Avenir',
-                            fontSize: 20,
-                            color: contentTextColor,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(height: 32),
-                        Divider(color: Colors.black38),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 32.0),
-                    child: Text(
-                      'Gallery (Need Internet Connection)',
-                      style: TextStyle(
-                        fontFamily: 'Avenir',
-                        fontSize: 25,
-                        color: const Color(0xff47455f),
-                        fontWeight: FontWeight.w300,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  Container(
-                    height: 250,
-                    padding: const EdgeInsets.only(left: 32.0),
-                    child: ListView.builder(
-                        itemCount: planetInfo.images.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            clipBehavior: Clip.antiAlias,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [gradientStartColor, gradientEndColor],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.3, 0.7])),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 300),
+                          Text(
+                            planetInfo.name,
+                            style: TextStyle(
+                              fontFamily: 'Avenir',
+                              fontSize: 56,
+                              color: titleTextColor,
+                              fontWeight: FontWeight.w900,
                             ),
-                            child: AspectRatio(
-                                aspectRatio: 1,
-                                child: Image.network(
-                                  planetInfo.images[index],
-                                  fit: BoxFit.cover,
-                                )),
-                          );
-                        }),
-                  ),
-                ],
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            'Solar System',
+                            style: TextStyle(
+                              fontFamily: 'Avenir',
+                              fontSize: 31,
+                              color: titleTextColor,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Divider(color: Colors.black38),
+                          SizedBox(height: 32),
+                          Text(
+                            planetInfo.description, //?? '',
+                            maxLines: 5,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontFamily: 'Avenir',
+                              fontSize: 20,
+                              color: titleTextColor,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(height: 32),
+                          Divider(color: Colors.black38),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 32.0),
+                      child: Text(
+                        'Gallery (Need Internet Connection)',
+                        style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontSize: 25,
+                          color: Colors.white, //const Color(0xff47455f),
+                          fontWeight: FontWeight.w100,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Container(
+                      height: 250,
+                      padding: const EdgeInsets.only(left: 32.0),
+                      child: ListView.builder(
+                          itemCount: planetInfo.images.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              clipBehavior: Clip.antiAlias,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                              child: AspectRatio(
+                                  aspectRatio: 1,
+                                  child: Image.network(
+                                    planetInfo.images[index],
+                                    fit: BoxFit.cover,
+                                  )),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
               ),
             ),
             Positioned(
